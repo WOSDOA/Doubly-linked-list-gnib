c7bd8e0aba5b6aed4a41316e58154cf86049b3ff
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2021-12-07 06:58:02 +0000

    iser-target: Fix implicit termination of connections

--- src/cryptopp/validat2.cpp
+++ src/cryptopp/validat2.cpp
@@ -90,4 +90,182 @@ bool ValidateBBS()
 	bbs.Seek(1234567);
 	bbs.GenerateBlock(buf, 20);
 	fail = memcmp(output2, buf, 20) != 0;
-	pass = pass &&
+	pass = pass && !fail;
+
+	cout << (fail ? "FAILED    " : "passed    ");
+	for (j=0;j<20;j++)
+		cout << setw(2) << setfill('0') << hex << (int)buf[j];
+	cout << endl;
+
+	return pass;
+}
+
+bool SignatureValidate(PK_Signer &priv, PK_Verifier &pub, bool thorough = false)
+{
+	bool pass = true, fail;
+
+	fail = !pub.GetMaterial().Validate(GlobalRNG(), thorough ? 3 : 2) || !priv.GetMaterial().Validate(GlobalRNG(), thorough ? 3 : 2);
+	pass = pass && !fail;
+
+	cout << (fail ? "FAILED    " : "passed    ");
+	cout << "signature key validation\n";
+
+	const byte *message = (byte *)"test message";
+	const int messageLen = 12;
+
+	SecByteBlock signature(priv.MaxSignatureLength());
+	size_t signatureLength = priv.SignMessage(GlobalRNG(), message, messageLen, signature);
+	fail = !pub.VerifyMessage(message, messageLen, signature, signatureLength);
+	pass = pass && !fail;
+
+	cout << (fail ? "FAILED    " : "passed    ");
+	cout << "signature and verification\n";
+
+	++signature[0];
+	fail = pub.VerifyMessage(message, messageLen, signature, signatureLength);
+	pass = pass && !fail;
+
+	cout << (fail ? "FAILED    " : "passed    ");
+	cout << "checking invalid signature" << endl;
+
+	if (priv.MaxRecoverableLength() > 0)
+	{
+		signatureLength = priv.SignMessageWithRecovery(GlobalRNG(), message, messageLen, NULL, 0, signature);
+		SecByteBlock recovered(priv.MaxRecoverableLengthFromSignatureLength(signatureLength));
+		DecodingResult result = pub.RecoverMessage(recovered, NULL, 0, signature, signatureLength);
+		fail = !(result.isValidCoding && result.messageLength == messageLen && memcmp(recovered, message, messageLen) == 0);
+		pass = pass && !fail;
+
+		cout << (fail ? "FAILED    " : "passed    ");
+		cout << "signature and verification with recovery" << endl;
+
+		++signature[0];
+		result = pub.RecoverMessage(recovered, NULL, 0, signature, signatureLength);
+		fail = result.isValidCoding;
+		pass = pass && !fail;
+
+		cout << (fail ? "FAILED    " : "passed    ");
+		cout << "recovery with invalid signature" << endl;
+	}
+
+	return pass;
+}
+
+bool CryptoSystemValidate(PK_Decryptor &priv, PK_Encryptor &pub, bool thorough = false)
+{
+	bool pass = true, fail;
+
+	fail = !pub.GetMaterial().Validate(GlobalRNG(), thorough ? 3 : 2) || !priv.GetMaterial().Validate(GlobalRNG(), thorough ? 3 : 2);
+	pass = pass && !fail;
+
+	cout << (fail ? "FAILED    " : "passed    ");
+	cout << "cryptosystem key validation\n";
+
+	const byte *message = (byte *)"test message";
+	const int messageLen = 12;
+	SecByteBlock ciphertext(priv.CiphertextLength(messageLen));
+	SecByteBlock plaintext(priv.MaxPlaintextLength(ciphertext.size()));
+
+	pub.Encrypt(GlobalRNG(), message, messageLen, ciphertext);
+	fail = priv.Decrypt(GlobalRNG(), ciphertext, priv.CiphertextLength(messageLen), plaintext) != DecodingResult(messageLen);
+	fail = fail || memcmp(message, plaintext, messageLen);
+	pass = pass && !fail;
+
+	cout << (fail ? "FAILED    " : "passed    ");
+	cout << "encryption and decryption\n";
+
+	return pass;
+}
+
+bool SimpleKeyAgreementValidate(SimpleKeyAgreementDomain &d)
+{
+	if (d.GetCryptoParameters().Validate(GlobalRNG(), 3))
+		cout << "passed    simple key agreement domain parameters validation" << endl;
+	else
+	{
+		cout << "FAILED    simple key agreement domain parameters invalid" << endl;
+		return false;
+	}
+
+	SecByteBlock priv1(d.PrivateKeyLength()), priv2(d.PrivateKeyLength());
+	SecByteBlock pub1(d.PublicKeyLength()), pub2(d.PublicKeyLength());
+	SecByteBlock val1(d.AgreedValueLength()), val2(d.AgreedValueLength());
+
+	d.GenerateKeyPair(GlobalRNG(), priv1, pub1);
+	d.GenerateKeyPair(GlobalRNG(), priv2, pub2);
+
+	memset(val1.begin(), 0x10, val1.size());
+	memset(val2.begin(), 0x11, val2.size());
+
+	if (!(d.Agree(val1, priv1, pub2) && d.Agree(val2, priv2, pub1)))
+	{
+		cout << "FAILED    simple key agreement failed" << endl;
+		return false;
+	}
+
+	if (memcmp(val1.begin(), val2.begin(), d.AgreedValueLength()))
+	{
+		cout << "FAILED    simple agreed values not equal" << endl;
+		return false;
+	}
+
+	cout << "passed    simple key agreement" << endl;
+	return true;
+}
+
+bool AuthenticatedKeyAgreementValidate(AuthenticatedKeyAgreementDomain &d)
+{
+	if (d.GetCryptoParameters().Validate(GlobalRNG(), 3))
+		cout << "passed    authenticated key agreement domain parameters validation" << endl;
+	else
+	{
+		cout << "FAILED    authenticated key agreement domain parameters invalid" << endl;
+		return false;
+	}
+
+	SecByteBlock spriv1(d.StaticPrivateKeyLength()), spriv2(d.StaticPrivateKeyLength());
+	SecByteBlock epriv1(d.EphemeralPrivateKeyLength()), epriv2(d.EphemeralPrivateKeyLength());
+	SecByteBlock spub1(d.StaticPublicKeyLength()), spub2(d.StaticPublicKeyLength());
+	SecByteBlock epub1(d.EphemeralPublicKeyLength()), epub2(d.EphemeralPublicKeyLength());
+	SecByteBlock val1(d.AgreedValueLength()), val2(d.AgreedValueLength());
+
+	d.GenerateStaticKeyPair(GlobalRNG(), spriv1, spub1);
+	d.GenerateStaticKeyPair(GlobalRNG(), spriv2, spub2);
+	d.GenerateEphemeralKeyPair(GlobalRNG(), epriv1, epub1);
+	d.GenerateEphemeralKeyPair(GlobalRNG(), epriv2, epub2);
+
+	memset(val1.begin(), 0x10, val1.size());
+	memset(val2.begin(), 0x11, val2.size());
+
+	if (!(d.Agree(val1, spriv1, epriv1, spub2, epub2) && d.Agree(val2, spriv2, epriv2, spub1, epub1)))
+	{
+		cout << "FAILED    authenticated key agreement failed" << endl;
+		return false;
+	}
+
+	if (memcmp(val1.begin(), val2.begin(), d.AgreedValueLength()))
+	{
+		cout << "FAILED    authenticated agreed values not equal" << endl;
+		return false;
+	}
+
+	cout << "passed    authenticated key agreement" << endl;
+	return true;
+}
+
+bool ValidateRSA()
+{
+	cout << "\nRSA validation suite running...\n\n";
+
+	byte out[100], outPlain[100];
+	bool pass = true, fail;
+
+	{
+		const char *plain = "Everyone gets Friday off.";
+		byte *signature = (byte *)
+			"\x05\xfa\x6a\x81\x2f\xc7\xdf\x8b\xf4\xf2\x54\x25\x09\xe0\x3e\x84"
+			"\x6e\x11\xb9\xc6\x20\xbe\x20\x09\xef\xb4\x40\xef\xbc\xc6\x69\x21"
+			"\x69\x94\xac\x04\xf3\x41\xb5\x7d\x05\x20\x2d\x42\x8f\xb2\xa2\x7b"
+			"\x5c\x77\xdf\xd9\xb1\x5b\xfc\x3d\x55\x93\x53\x50\x34\x10\xc1\xe1";
+
+		FileSource keys("TestData/rsa512a.dat", true, new HexDe
