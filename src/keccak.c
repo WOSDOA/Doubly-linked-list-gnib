c1da5ba532031ddcc123c954483bdcd34a5f019b
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2021-12-21 12:44:49 +0000

    iio: DAC: ad5624r_spi: fix bit shift of output data value

---
+++ src/keccak.c
@@ -0,0 +1,13 @@
+/* $Id: keccak.c 259 2011-07-19 22:11:27Z tp $ */
+/*
+ * Keccak implementation.
+ *
+ * ==========================(LICENSE BEGIN)============================
+ *
+ * Copyright (c) 2007-2010  Projet RNRT SAPHIR
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * 
