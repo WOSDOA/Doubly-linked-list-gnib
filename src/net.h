14160659ab285521316a80a73a1f77abb6a7e6a5
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2022-01-19 11:12:30 +0000

    pulled translations from transifex

---
+++ src/net.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BITCOIN_NET_H
+#define BITCOIN_NET_H
+
+#include <deque>
+#include <boost/array.hpp>
+#include <boost/foreach.hpp>
+#include <openssl/rand.h>
+
+#ifndef WIN32
+#include <arpa/inet.h>
+#endif
+
+#include "mruset.h"
+#include "limitedmap.h"
+#include "netbase.h"
+#include "protocol.h"
+#include "addrman.h"
+#include "hash.h"
+#include "bloom.h"
+
+class CNode;
+class CBlockIndex;
+extern int nBestHeight;
+
+
+
+inline unsigned int ReceiveFloodSize() { return 1000*GetArg("-maxreceivebuffer", 5*1000); }
+inline unsigned int SendBufferSize() { return 1000*GetArg("-maxsendbuffer", 1*1000); }
+
+void AddOneShot(std::string strDest);
+bool RecvLine(SOCKET hSocket, std::string& strLine);
+bool GetMyExternalIP(CNetAddr& ipRet);
+void AddressCurrentlyConnected(const CService& addr);
+CNode* FindNode(const CNetAddr& ip);
+CNode* FindNode(const CService& ip);
+CNode* ConnectNode(CAddress addrConnect, const char *strDest = NULL);
+void MapPort(bool fUseUPnP);
+unsign
