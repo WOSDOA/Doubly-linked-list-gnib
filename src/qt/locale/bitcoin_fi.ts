08cca479f7b2e12cf4bb5765c3eeb70ff817dbb2
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2022-02-25 11:24:41 +0000

    Security

---
+++ src/qt/locale/bitcoin_fi.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.0">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="+14"/>
+        <source>About MaxCoin</source>
+        <translation>Tietoa Bitcoinista</translation>
+    </message>
+    <message>
+        <location line="+39"/>
+        <source>&lt;b&gt;MaxCoin&lt;/b&gt; version</source>
+        <translation>&lt;b&gt;MaxCoin&lt;/b&gt; versio</translation>
+    </message>
+    <message>
+        <location line="+57"/>
+        <source>
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+This product includes software developed by the OpenS
