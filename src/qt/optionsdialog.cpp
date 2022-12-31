513f8ce3994ef013d9390257810e5354c03aa005
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2022-12-31 09:51:37 +0000

    Update history.

---
+++ src/qt/optionsdialog.cpp
@@ -0,0 +1,101 @@
+#include "optionsdialog.h"
+#include "ui_optionsdialog.h"
+
+#include "bitcoinunits.h"
+#include "monitoreddatamapper.h"
+#include "netbase.h"
+#include "optionsmodel.h"
+
+#include <QDir>
+#include <QIntValidator>
+#include <QLocale>
+#include <QMessageBox>
+
+OptionsDialog::OptionsDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::OptionsDialog),
+    model(0),
+    mapper(0),
+    fRestartWarningDisplayed_Proxy(false),
+    fRestartWarningDisplayed_Lang(false),
+    fProxyIpValid(true)
+{
+    ui->setupUi(this);
+
+    /* Network elements init */
+#ifndef USE_UPNP
+    ui->mapPortUpnp->setEnabled(false);
+#endif
+
+    ui->proxyIp->setEnabled(false);
+    ui->proxyPort->setEnabled(false);
+    ui->proxyPort->setValidator(new QIntValidator(1, 65535, this));
+
+    ui->socksVersion->setEnabled(false);
+    ui->socksVersion->addItem("5", 5);
+    ui->socksVersion->addItem("4", 4);
+    ui->socksVersion->setCurrentIndex(0);
+
+    connect(ui->connectSocks, SIGNAL(toggled(bool)), ui->proxyIp, SLOT(setEnabled(bool)));
+    connect(ui->connectSocks, SIGNAL(toggled(bool)), ui->proxyPort, SLOT(setEnabled(bool)));
+    connect(ui->connectSocks, SIGNAL(toggled(bool)), ui->socksVersion, SLOT(setEnabled(bool)));
+    connect(ui->connectSocks, SIGNAL(clicked(bool)), this, SLOT(showRestartWarning_Proxy()));
+
+    ui->proxyIp->installEventFilter(this);
+
+    /* Window elements init */
+#ifdef Q_OS_MAC
+    ui->tabWindow->setVisible(false);
+#endif
+
+    /* Display elements init */
+    QDir translations(":translations");
+    ui->lang->addItem(QString("(") + tr("default") + QString(")"), QVariant(""));
+    foreach(const QString &langStr, translations.entryList())
+    {
+        QLocale locale(langStr);
+
+        /** check if the locale name consists of 2 parts (language_country) */
+        if(langStr.contains("_"))
+        {
+#if QT_VERSION >= 0x040800
+            /** display language strings as "native language - native country (locale name)", e.g. "Deutsch - Deutschland (de)" */
+            ui->lang->addItem(locale.nativeLanguageName() + QString(" - ") + locale.nativeCountryName() + QString(" (") + langStr + QString(")"), QVariant(langStr));
+#else
+            /** display language strings as "language - country (locale name)", e.g. "German - Germany (de)" */
+            ui->lang->addItem(QLocale::languageToString(locale.language()) + QString(" - ") + QLocale::countryToString(locale.country()) + QString(" (") + langStr + QString(")"), QVariant(langStr));
+#endif
+        }
+        else
+        {
+#if QT_VERSION >= 0x040800
+            /** display language strings as "native language (locale name)", e.g. "Deutsch (de)" */
+            ui->lang->addItem(locale.nativeLanguageName() + QString(" (") + langStr + QString(")"), QVariant(langStr));
+#else
+            /** display language strings as "language (locale name)", e.g. "German (de)" */
+            ui->lang->addItem(QLocale::languageToString(locale.language()) + QString(" (") + langStr + QString(")"), QVariant(langStr));
+#endif
+        }
+    }
+
+    ui->unit->setModel(new BitcoinUnits(this));
+
+    /* Widget-to-option mapper */
+    mapper = new MonitoredDataMapper(this);
+    mapper->setSubmitPolicy(QDataWidgetMapper::ManualSubmit);
+    mapper->setOrientation(Qt::Vertical);
+
+    /* enable apply button when data modified */
+    connect(mapper, SIGNAL(viewModified()), this, SLOT(enableApplyButton()));
+    /* disable apply button when new data loaded */
+    connect(mapper, SIGNAL(currentIndexChanged(int)), this, SLOT(disableApplyButton()));
+    /* setup/change UI elements when proxy IP is invalid/valid */
+    connect(this, SIGNAL(proxyIpValid(QValidatedLineEdit *, bool)), this, SLOT(handleProxyIpValid(QValidatedLineEdit *, bool)));
+}
+
+OptionsDialog::~OptionsDialog()
+{
+    delete ui;
+}
+
+void OptionsDialog::setModel(OptionsModel
