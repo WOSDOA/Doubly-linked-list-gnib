bde38d0e9e23ce90cb65ae7887d01534b83014ed
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2023-01-07 17:44:01 +0000

    update module version

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -98,4 +98,183 @@ OptionsDialog::~OptionsDialog()
     delete ui;
 }
 
-void OptionsDialog::setModel(OptionsModel
+void OptionsDialog::setModel(OptionsModel *model)
+{
+    this->model = model;
+
+    if(model)
+    {
+        connect(model, SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
+
+        mapper->setModel(model);
+        setMapper();
+        mapper->toFirst();
+    }
+
+    /* update the display unit, to not use the default ("MAX") */
+    updateDisplayUnit();
+
+    /* warn only when language selection changes by user action (placed here so init via mapper doesn't trigger this) */
+    connect(ui->lang, SIGNAL(valueChanged()), this, SLOT(showRestartWarning_Lang()));
+
+    /* disable apply button after settings are loaded as there is nothing to save */
+    disableApplyButton();
+}
+
+void OptionsDialog::setMapper()
+{
+    /* Main */
+    mapper->addMapping(ui->transactionFee, OptionsModel::Fee);
+    mapper->addMapping(ui->bitcoinAtStartup, OptionsModel::StartAtStartup);
+
+    /* Network */
+    mapper->addMapping(ui->mapPortUpnp, OptionsModel::MapPortUPnP);
+
+    mapper->addMapping(ui->connectSocks, OptionsModel::ProxyUse);
+    mapper->addMapping(ui->proxyIp, OptionsModel::ProxyIP);
+    mapper->addMapping(ui->proxyPort, OptionsModel::ProxyPort);
+    mapper->addMapping(ui->socksVersion, OptionsModel::ProxySocksVersion);
+
+    /* Window */
+#ifndef Q_OS_MAC
+    mapper->addMapping(ui->minimizeToTray, OptionsModel::MinimizeToTray);
+    mapper->addMapping(ui->minimizeOnClose, OptionsModel::MinimizeOnClose);
+#endif
+
+    /* Display */
+    mapper->addMapping(ui->lang, OptionsModel::Language);
+    mapper->addMapping(ui->unit, OptionsModel::DisplayUnit);
+    mapper->addMapping(ui->displayAddresses, OptionsModel::DisplayAddresses);
+}
+
+void OptionsDialog::enableApplyButton()
+{
+    ui->applyButton->setEnabled(true);
+}
+
+void OptionsDialog::disableApplyButton()
+{
+    ui->applyButton->setEnabled(false);
+}
+
+void OptionsDialog::enableSaveButtons()
+{
+    /* prevent enabling of the save buttons when data modified, if there is an invalid proxy address present */
+    if(fProxyIpValid)
+        setSaveButtonState(true);
+}
+
+void OptionsDialog::disableSaveButtons()
+{
+    setSaveButtonState(false);
+}
+
+void OptionsDialog::setSaveButtonState(bool fState)
+{
+    ui->applyButton->setEnabled(fState);
+    ui->okButton->setEnabled(fState);
+}
+
+void OptionsDialog::on_resetButton_clicked()
+{
+    if(model)
+    {
+        // confirmation dialog
+        QMessageBox::StandardButton btnRetVal = QMessageBox::question(this, tr("Confirm options reset"),
+            tr("Some settings may require a client restart to take effect.") + "<br><br>" + tr("Do you want to proceed?"),
+            QMessageBox::Yes | QMessageBox::Cancel, QMessageBox::Cancel);
+
+        if(btnRetVal == QMessageBox::Cancel)
+            return;
+
+        disableApplyButton();
+
+        /* disable restart warning messages display */
+        fRestartWarningDisplayed_Lang = fRestartWarningDisplayed_Proxy = true;
+
+        /* reset all options and save the default values (QSettings) */
+        model->Reset();
+        mapper->toFirst();
+        mapper->submit();
+
+        /* re-enable restart warning messages display */
+        fRestartWarningDisplayed_Lang = fRestartWarningDisplayed_Proxy = false;
+    }
+}
+
+void OptionsDialog::on_okButton_clicked()
+{
+    mapper->submit();
+    accept();
+}
+
+void OptionsDialog::on_cancelButton_clicked()
+{
+    reject();
+}
+
+void OptionsDialog::on_applyButton_clicked()
+{
+    mapper->submit();
+    disableApplyButton();
+}
+
+void OptionsDialog::showRestartWarning_Proxy()
+{
+    if(!fRestartWarningDisplayed_Proxy)
+    {
+        QMessageBox::warning(this, tr("Warning"), tr("This setting will take effect after restarting MaxCoin."), QMessageBox::Ok);
+        fRestartWarningDisplayed_Proxy = true;
+    }
+}
+
+void OptionsDialog::showRestartWarning_Lang()
+{
+    if(!fRestartWarningDisplayed_Lang)
+    {
+        QMessageBox::warning(this, tr("Warning"), tr("This setting will take effect after restarting MaxCoin."), QMessageBox::Ok);
+        fRestartWarningDisplayed_Lang = true;
+    }
+}
+
+void OptionsDialog::updateDisplayUnit()
+{
+    if(model)
+    {
+        /* Update transactionFee with the current unit */
+        ui->transactionFee->setDisplayUnit(model->getDisplayUnit());
+    }
+}
+
+void OptionsDialog::handleProxyIpValid(QValidatedLineEdit *object, bool fState)
+{
+    // this is used in a check before re-enabling the save buttons
+    fProxyIpValid = fState;
+
+    if(fProxyIpValid)
+    {
+        enableSaveButtons();
+        ui->statusLabel->clear();
+    }
+    else
+    {
+        disableSaveButtons();
+        object->setValid(fProxyIpValid);
+        ui->statusLabel->setStyleSheet("QLabel { color: red; }");
+        ui->statusLabel->setText(tr("The supplied proxy address is invalid."));
+    }
+}
+
+bool OptionsDialog::eventFilter(QObject *object, QEvent *event)
+{
+    if(event->type() == QEvent::FocusOut)
+    {
+        if(object == ui->proxyIp)
+        {
+            CService addr;
+            /* Check proxyIp for a valid IPv4/IPv6 address and emit the proxyIpValid signal */
+            emit proxyIpValid(ui->proxyIp, LookupNumeric(ui->proxyIp->text().toStdString().c_str(), addr));
+        }
+    }
+    return QDialog::eventFilter(object, event);
+}
