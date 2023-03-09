8b7f963fbca2f36a128d513601321492d1027106
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2023-03-09 19:48:18 +0000

    Correct name

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -87,4 +87,13 @@ Value dumpprivkey(const Array& params, bool fHelp)
     string strAddress = params[0].get_str();
     CBitcoinAddress address;
     if (!address.SetString(strAddress))
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid MaxCoin 
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid MaxCoin address");
+    CKeyID keyID;
+    if (!address.GetKeyID(keyID))
+        throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
+    CSecret vchSecret;
+    bool fCompressed;
+    if (!pwalletMain->GetSecret(keyID, vchSecret, fCompressed))
+        throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
+    return CBitcoinSecret(vchSecret, fCompressed).ToString();
+}
