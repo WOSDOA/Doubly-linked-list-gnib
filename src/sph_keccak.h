f82a090e950c1c8f3a075a95632762aeefeac97e
WOSDOA <95955832+WOSDOA@users.noreply.github.com>
2023-03-20 21:55:35 +0000

    make pretty

---
+++ src/sph_keccak.h
@@ -0,0 +1,7 @@
+/* $Id: sph_keccak.h 216 2010-06-08 09:46:57Z tp $ */
+/**
+ * Keccak interface. This is the interface for Keccak with the
+ * recommended parameters for SHA-3, with output lengths 224, 256,
+ * 384 and 512 bits.
+ *
+ * ================
